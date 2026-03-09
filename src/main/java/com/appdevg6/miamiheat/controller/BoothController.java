129735f743a5ef52640373bdf97b8bba2a647a75
Earl Elorde <cpt.woof16@gmail.com>
2026-03-09 21:45:37 +0800

    Update BoothController.java

--- src/main/java/com/appdevg6/miamiheat/controller/BoothController.java
+++ src/main/java/com/appdevg6/miamiheat/controller/BoothController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/api/booth")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class BoothController {
 
     @Autowired
