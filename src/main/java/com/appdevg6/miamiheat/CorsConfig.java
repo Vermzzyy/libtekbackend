29bb436b977c9e222a4cb3d2d11ab12c1f6b6f81
Vermzzyy <cpt.woof16@gmail.com>
2026-04-29 19:20:56 +0800

    fixed

--- src/main/java/com/appdevg6/miamiheat/CorsConfig.java
+++ src/main/java/com/appdevg6/miamiheat/CorsConfig.java
@@ -1,3 +1,5 @@
+package com.appdevg6.miamiheat;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
